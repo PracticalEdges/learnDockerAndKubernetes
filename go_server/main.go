79db6352ce2619eaf2f38207a531fe06e5675355
siddh34 <ssiddharthsutar@gmail.com>
2024-08-22 16:38:41 +0530

    added jokes

--- go_server/main.go
+++ go_server/main.go
@@ -17,7 +17,14 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler).Methods("GET")
 	log.Println("Server started on port 8080")
-	r.HandleFunc("/books", router.GetAllBooksHandler).Methods("GET")
 
-	http.ListenAndServe(":8080", r)
+	// books routes
+	r.HandleFunc("/books", router.GetAllBooksHandler).Methods("GET")
+	r.HandleFunc("/books/{id}", router.GetSingleBook).Methods("GET")
+
+	// Jokes routes
+	r.HandleFunc("/jokes", router.GetAllJokes).Methods("GET")
+	r.HandleFunc("/jokes/{id}", router.GetSingleJoke).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
--- go_server/router/jokes.go
+++ go_server/router/jokes.go
@@ -1,2 +1,105 @@
 package router
 
+import (
+	"encoding/json"
+	"goserver/models"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/gorilla/mux"
+)
+
+func GetAllJokes(w http.ResponseWriter, r *http.Request) {
+	r.Header.Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
+
+	absPath, err := filepath.Abs("./data/data.json")
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error getting absolute path", http.StatusInternalServerError)
+		return
+	}
+
+	jsonFile, err := os.Open(absPath)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error opening file", http.StatusInternalServerError)
+		return
+	}
+
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		http.Error(w, "Error reading file", http.StatusInternalServerError)
+		return
+	}
+
+	var data models.Data
+	err = json.Unmarshal(byteValue, &data)
+	if err != nil {
+		http.Error(w, "Error unmarshalling json", http.StatusInternalServerError)
+		return
+	}
+
+	jokes := data.Jokes
+
+	jsonJokes, err := json.Marshal(jokes)
+	if err != nil {
+		http.Error(w, "Error marshalling jokes", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(jsonJokes)
+
+	defer jsonFile.Close()
+}
+
+func GetSingleJoke(w http.ResponseWriter, r *http.Request) {
+	r.Header.Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
+
+	absPath, err := filepath.Abs("./data/data.json")
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error getting absolute path", http.StatusInternalServerError)
+		return
+	}
+
+	jsonFile, err := os.Open(absPath)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error opening file", http.StatusInternalServerError)
+		return
+	}
+
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		http.Error(w, "Error reading file", http.StatusInternalServerError)
+		return
+	}
+
+	var data models.Data
+	err = json.Unmarshal(byteValue, &data)
+	if err != nil {
+		http.Error(w, "Error unmarshalling json", http.StatusInternalServerError)
+		return
+	}
+
+	jokes := data.Jokes
+
+	vars := mux.Vars(r)
+	joke := jokes[vars["id"]]
+
+	jsonJoke, err := json.Marshal(joke)
+
+	if err != nil {
+		http.Error(w, "Error marshalling joke", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(jsonJoke)
+
+	defer jsonFile.Close()
+}
