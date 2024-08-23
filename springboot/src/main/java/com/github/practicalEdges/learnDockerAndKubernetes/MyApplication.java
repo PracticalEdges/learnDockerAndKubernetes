39634d05f64231789ab3fb521fdf7f8ec92f9be8
siddh34 <ssiddharthsutar@gmail.com>
2024-08-23 17:54:08 +0530

    working springboot

--- springboot/src/main/java/com/github/practicalEdges/learnDockerAndKubernetes/MyApplication.java
+++ springboot/src/main/java/com/github/practicalEdges/learnDockerAndKubernetes/MyApplication.java
@@ -1,12 +1,15 @@
+package com.github.practicalEdges.learnDockerAndKubernetes;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, R2dbcAutoConfiguration.class })
 public class MyApplication {
-
 	@RequestMapping("/")
 	String home() {
 		return "Hello World!";
@@ -15,5 +18,4 @@ public class MyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
 	}
-
 }
