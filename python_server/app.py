2c3c22655e01c20c7c1cf1556923b11566c16d94
siddh34 <ssiddharthsutar@gmail.com>
2024-09-09 19:14:59 +0530

    feat: added unit test for flask

--- python_server/app.py
+++ python_server/app.py
@@ -1,14 +1,20 @@
+# app.py
 from flask import Flask
-from router.pokemon import poke_bp
-
-app = Flask(__name__)
-
-@app.route("/")
-def starting_msg():
-    return {"message": "hello you can search pokemon here"}
+from .router.pokemon import poke_bp
 
 
-app.register_blueprint(poke_bp, url_prefix="/pokemon")
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def starting_msg():
+        return {"message": "hello you can search pokemon here"}
+
+    app.register_blueprint(poke_bp, url_prefix="/pokemon")
+
+    return app
+
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True, host="0.0.0.0", port=5000)
--- python_server/tests/test_pokemon.py
+++ python_server/tests/test_pokemon.py
@@ -1,27 +1,8 @@
-# conftest.py
 import pytest
-from your_flask_app import (
-    create_app,
-)  # Adjust the import according to your app structure
+from ..app import create_app
 
 
-@pytest.fixture
-def app():
-    app = create_app()
-    app.config.update(
-        {
-            "TESTING": True,
-        }
-    )
-
-    yield app
-
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
-
-
-@pytest.fixture
-def runner(app):
-    return app.test_cli_runner()
+def test_root(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json == {"message": "hello you can search pokemon here"}
