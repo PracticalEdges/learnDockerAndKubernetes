c0b3446148f498dd274eb965a84764258481d945
siddh34 <ssiddharthsutar@gmail.com>
2024-09-09 19:29:15 +0530

    feat: added node tests

--- python_server/tests/test_pokemon.py
+++ python_server/tests/test_pokemon.py
@@ -1,7 +1,6 @@
 import pytest
 from ..app import create_app
 
-
 def test_root(client):
     response = client.get("/")
     assert response.status_code == 200
--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, act } from "@testing-library/react";
+import React from "react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders elements", () => {
+	act(() => {
+		render(<App />);
+	});
+
+	// check buttons
+	const linkElement = screen.getByText(/Modify Users/i);
+	expect(linkElement).toBeInTheDocument();
+
+	const linkElement2 = screen.getByText(/Add Users/i);
+	expect(linkElement2).toBeInTheDocument();
+
+	const linkElement3 = screen.getByText(/Delete Users/i);
+	expect(linkElement3).toBeInTheDocument();
+
+	// check table
+	const table = screen.getByRole("table");
+	expect(table).toBeInTheDocument();
 });
